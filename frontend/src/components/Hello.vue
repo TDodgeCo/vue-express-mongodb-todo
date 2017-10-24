af8e9250543578231bdc77a1edb99eefdf1943b8
Tim Dodge <tim.d@gwbuildings.com>
2017-10-24 16:51:21 -0600

    added axios and tested a quick connection to the server

---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2 v-for="task in tasks">{{task.name}}</h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      tasks: {}
+    }
+  },
+  mounted () {
+    this.getTasks()
+  },
+  methods: {
+    getTasks: function () {
+      axios.get('/api/tasks').then(response => {
+        this.tasks = response.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
