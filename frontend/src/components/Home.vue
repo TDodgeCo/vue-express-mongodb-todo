51b94f185491e55dfe46d32c87a491e602b6db9b
Tim Dodge <timldodge@gmail.com>
2017-10-28 18:19:18 -0600

    added bootstrap 4

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,35 +1,11 @@
 <template>
-  <v-tabs dark fixed centered>
-    <v-toolbar color="cyan">
-      <v-text-field
-        solo
-        label="Search"
-        append-icon="keyboard_voice"
-        prepend-icon="search"
-      ></v-text-field>
-      <v-tabs-bar class="cyan" slot="extension">
-        <v-tabs-slider color="yellow"></v-tabs-slider>
-        <v-tabs-item
-          v-for="i in 3"
-          :key="i"
-          :href="'#tab-' + i"
-        >
-          Item {{ i }}
-        </v-tabs-item>
-      </v-tabs-bar>
-    </v-toolbar>
-    <v-tabs-items>
-      <v-tabs-content
-        v-for="i in 3"
-        :key="i"
-        :id="'tab-' + i"
-      >
-        <v-card flat>
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
-      </v-tabs-content>
-    </v-tabs-items>
-  </v-tabs>
+  <div class="container-fluid">
+    <div class="row justify-content-start">
+      <div class="col-4">
+        Hello World
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
